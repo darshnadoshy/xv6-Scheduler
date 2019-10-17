523ef5c0d71afdeb86601b1b9c83a4145c434b58
DARSHNA Doshy <darshna@royal-01.cs.wisc.edu>
2019-10-17 18:45:26 -0500

    Syscalls added

--- xv6/proc.c
+++ xv6/proc.c
@@ -221,6 +221,77 @@ fork(void)
   return pid;
 }
 
+int
+fork2(int pri)
+{
+  int i, pid;
+  struct proc *np;
+  struct proc *curproc = myproc();
+
+  if(pri < 0 || pri > 3)
+  {
+    return -1;
+  }
+
+  // Allocate process.
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  // Copy process state from proc.
+  if((np->pgdir = copyuvm(curproc->pgdir, curproc->sz)) == 0){
+    kfree(np->kstack);
+    np->kstack = 0;
+    np->state = UNUSED;
+    return -1;
+  }
+  np->sz = curproc->sz;
+  np->parent = curproc;
+  *np->tf = *curproc->tf;
+
+  // Clear %eax so that fork returns 0 in the child.
+  np->tf->eax = 0;
+
+  for(i = 0; i < NOFILE; i++)
+    if(curproc->ofile[i])
+      np->ofile[i] = filedup(curproc->ofile[i]);
+  np->cwd = idup(curproc->cwd);
+
+  safestrcpy(np->name, curproc->name, sizeof(curproc->name));
+
+  pid = np->pid;
+
+  acquire(&ptable.lock);
+
+  np->state = RUNNABLE;
+
+  release(&ptable.lock);
+
+  return pid;
+}
+
+int getpri(int PID)
+{
+  int flag = 0;
+  for(int i = 0; i < NPROC; i++)
+  {
+    if(ps.pid[i] == PID)
+    {
+      flag = 1;
+      return ps.priority[i];
+    }
+  }
+  if(flag == 0)
+  {
+    return -1;
+  }
+}
+
+int setpri(int PID, int pri)
+{
+  int 
+  
+}
 // Exit the current process.  Does not return.
 // An exited process remains in the zombie state
 // until its parent calls wait() to find out it exited.
