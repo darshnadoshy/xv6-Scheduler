9d70e89f597abe25ec83af327343a318c576d80d
Kushal Patel <kushal@snares-04.cs.wisc.edu>
2019-10-18 22:06:02 -0500

    userRR.c updated

--- xv6/userRR.c
+++ xv6/userRR.c
@@ -9,8 +9,13 @@ main(int argc, char *argv[])
     printf(2, "Usage:  userRR <user-level-timeslice> <iterations> <job> <jobcount>\n");
     exit();
   }
+  // Initialize the values from cmd line args
   int user_timeslice = atoi(argv[1]);
   int iterations = atoi(argv[2]);
+  char job[16];
+  strcpy(job, argv[3]);
+  int job_count = atoi(argv[4]);
+  
   
 
   exit();
