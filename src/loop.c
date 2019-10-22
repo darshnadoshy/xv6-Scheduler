235733003314b619c7daf2a42489b199164fc3ad
DARSHNA Doshy <darshna@royal-04.cs.wisc.edu>
2019-10-21 19:22:30 -0500

    Changes made to userRR and loop

--- src/loop.c
+++ src/loop.c
@@ -6,10 +6,11 @@
 int
 main(int argc, char *argv[])
 {
-  for(int i = 0; i < 10; i++)
-  {
-  	sleep(10);
-  }
+  // for(int i = 0; i < 10; i++)
+  // {
+  //   sleep(10);
+  // }
+  sleep(10);
   printf(1,"%d\n", getpid());
   exit();
 }
--- src/userRR.c
+++ src/userRR.c
@@ -6,9 +6,10 @@
 int
 main(int argc, char *argv[])
 {
-  int user_timeslice, iterations, job_count;
+  int user_timeslice, job_count; //, iterations;
   char *job[64];
-
+  struct pstat ps;
+  
   if(argc > 5 || argc < 5 ){
     printf(2, "Usage:  userRR <user-level-timeslice> <iterations> <job> <jobcount>\n");
     exit();
@@ -16,7 +17,7 @@ main(int argc, char *argv[])
   
   // Initialize the values from cmd line args
   user_timeslice = atoi(argv[1]);
-  iterations = atoi(argv[2]);
+  // iterations = atoi(argv[2]);
   job_count = atoi(argv[4]);
 
    for(int i = 0; i < job_count; i++)
@@ -30,35 +31,59 @@ main(int argc, char *argv[])
     for(int i = 0; i < 64; i++) {
       np[i] = 0;
     }
-    // setpri(curproc->pid, 3);
     setpri(getpid(), 3);
-    for(int i = 0; i < job_count; i++)
+
+    for(int i = 0; i < job_count ; i++)
     {
       np[i]->pid = fork2(0);
+      printf(1, "np[%d]->pid = %d\n", i, np[i]->pid);
       if(np[i]->pid < 0) {
         printf(2, "Could not fork!\n");
         exit();
       }
-      if(exec(job[0], job) < 0)
+      if(np[i]->pid == 0)
+      {
+        if(exec(job[0], job) < 0)
         {
           printf(2, "Exec failed!\n");
         }
         exit();
-    }
-
-    for(int i = 0; i < iterations; i++)
-    {
-      for(int j = 0; j < job_count; j++)
+      }
+      else
       {
-        setpri(np[j]->pid, 2);
-        sleep(user_timeslice);
-        setpri(np[j]->pid, 0);
+        //sleep(user_timeslice);
+        wait();
       }
     }
 
-    for(int i = 0; i < job_count; i++)
+    // for(int i = 0; i < iterations; i++)
+    // {
+    //   for(int j = 0; j < job_count; j++)
+    //   {
+    //      setpri(np[j]->pid, 2);
+    //      sleep(user_timeslice);
+    //      setpri(np[j]->pid, 0);
+    //      printf(1, "jobcount = %d %d\n", j, job_count);
+    //   }
+    // }
+
+    for(int i = 0; i < user_timeslice; i++)
     {
-      kill(np[i]->pid);
+      wait();
+    }
+    getpinfo(&ps);
+    for(int i = 0; i < job_count + 3; i++)
+    {
+      //printf(1, "Inuse: %d\n", ps.inuse[i]);
+      printf(1, "Pid: %d\n", ps.pid[i]);
+      
+      //printf(1, "State: %d\n", ps.state[i]);
+      for(int j = 0; j < 4; j++)
+      {
+        printf(1, "Priority: %d\n", j); // ps.priority[j]);
+        printf(1, "Ticks: %d\n", ps.ticks[i][j]);
+        printf(1, "Qtail: %d\n", ps.qtail[i][j]);
+      }    
     }
   
   exit();
